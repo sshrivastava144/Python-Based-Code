e7322be51bf7473895315ab22b9b180f54b33a60
sshrivastava144 <105547537+sshrivastava144@users.noreply.github.com>
2024-09-14 17:09:08 +0530

    Add files via upload

---
+++ Replace_value_using_for_loop.py
@@ -0,0 +1,10 @@
+# write a program the replace a value without using any method
+
+str1='shivam'
+str2=''
+for i in str1:
+    if i!='s':
+        str2=str2+i
+    else:
+        str2=str2+'l'
+print(str2)
