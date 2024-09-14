9da592b3195fc6f6c8daa5a79914756e956b6bda
sshrivastava144 <105547537+sshrivastava144@users.noreply.github.com>
2024-09-14 17:01:49 +0530

    Add files via upload

---
+++ Remove_all_white_space.py
@@ -0,0 +1,13 @@
+# count the word
+'''str1='shivam shrivastava'
+count1=0
+for i in str1:
+    count1=count1+1
+print(count1)'''
+
+# write the program to count of character
+'''str1='shivamshrivastava'
+dict1={}
+count1=0
+for i in str1:
+    print('shivam shrivastava')'''
